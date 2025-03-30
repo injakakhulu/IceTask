d463c75b61a5a6052f215b7458827ae7d56cb84d
injakakhulu <st10464050@rcconnect.edu.za>
2025-03-30 21:54:10 +0000

    iceTask2

--- src/IceTask2/IceTask2.java
+++ src/IceTask2/IceTask2.java
@@ -14,7 +14,7 @@ import java.util.*;
 public class IceTask2 {
 
     /**
-     * @param args the command line arguments
+     * @param args the command line argument
      */
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
